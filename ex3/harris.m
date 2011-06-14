5c3688d00a66b8d85fe69cca9a6260bc0ea0b53b
maradatscha <skoch@gris.informatik.tu-darmstadt.de>
2011-06-14 16:21:17 +0200

    blub

--- ex3/harris.m
+++ ex3/harris.m
@@ -1,7 +1,7 @@
 function ha = harris(c, s, a)
 
     s2 = s*s;
-ha = zeros(size(c,1), size(c,2));
+    ha = zeros(size(c,1), size(c,2));
 
     for x =1:size(c,1)
         for y =1:size(c,2)
@@ -9,4 +9,5 @@ ha = zeros(size(c,1), size(c,2));
             ha(x,y) = det(s2.*t)-a*trace(t)^2;
         end
     end
+    
 end
