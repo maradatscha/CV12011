2b1c9672611956e831422bb7212415311ce61bc8
Fabian Langguth <fabian.langguth@gmail.com>
2011-07-08 17:26:12 +0200

    added eightpoint

---
+++ ex4/eightpoint.m
@@ -0,0 +1,59 @@
+function F = eightpoint(pt1, pt2)
+
+assert(size(pt1,1) == 8 );
+assert(size(pt1,2) == 2 );
+
+assert(size(pt2,1) == 8 );
+assert(size(pt2,2) == 2 );
+
+
+% compute normalization matrices
+norms = sqrt(sum(abs(pt1).^2,2));
+s = 0.5 * max(norms);
+t = mean(pt1);
+
+T1 = [ 1/s  0  -t(1)/s ;
+        0  1/s -t(2)/s ;
+	    0   0      1   ];
+
+norms = sqrt(sum(abs(pt2).^2,2));
+s = 0.5 * max(norms);
+t = mean(pt2);
+
+T2 = [ 1/s  0  -t(1)/s ;
+        0  1/s -t(2)/s ;
+	    0   0      1   ];
+
+ut1 = T1 * [pt1 ones(size(pt1,1),1)]';
+ut2 = T2 * [pt2 ones(size(pt2,1),1)]';
+
+% build equation system 
+A = zeros(8,9);
+for i = 1:8
+	A(i,1) = ut1(1,i)*ut2(1,i);
+	A(i,2) = ut1(2,i)*ut2(1,i);
+	A(i,3) =          ut2(1,i);
+	A(i,4) = ut1(1,i)*ut2(2,i);
+	A(i,5) = ut1(2,i)*ut2(2,i);
+	A(i,6) =          ut2(2,i);
+	A(i,7) = ut1(1,i);
+	A(i,8) = ut1(2,i);
+	A(i,9) = 1;
+end
+
+% solve for F~
+[U S V] = svd(A,0);
+Ft = [ V(1,9) V(2,9) V(3,9) ;
+	   V(4,9) V(5,9) V(6,9) ;
+	   V(7,9) V(8,9) V(9,9) ];
+
+% enforce rank 2 for F_
+[U S V] = svd(Ft,0);
+S(3,3) = 0;
+Fb = U*S*V';
+
+% transform F
+F = T2' * Fb * T1;
+
+
+
---
+++ ex4/two_view.m
@@ -0,0 +1,49 @@
+clear all;
+close all;
+
+pt1 = [
+    45   210
+   253   211
+   154   188
+    27    37
+   209   164
+    33    77
+    93    58
+    66    75  
+];
+
+pt2 = [
+    87   216
+   285   216
+   188   194
+    51    49
+   234   171
+    56    88
+   114    69
+    87    86
+];
+
+img1 = imread('image1.png');
+img2 = imread('image2.png');
+
+imgf = [img1 img2];
+
+% show images and check correspondences
+imshow(imgf);
+hold on;
+dim1 = size(img1,2);
+plot(pt1(:,1),pt1(:,2), 'rs');
+plot(pt2(:,1)+dim1,pt2(:,2), 'bs');
+for i = 1:size(pt1,1)
+	plot([pt1(i,1) pt2(i,1)+dim1], [pt1(i,2) pt2(i,2)], 'y-');
+end
+
+F = eightpoint(pt1,pt2);
+
+% errors for all 8 points
+for i = 1:8
+	[pt1(i,:) 1] * F * [pt2(i,:) 1]'
+end
+
+
+
